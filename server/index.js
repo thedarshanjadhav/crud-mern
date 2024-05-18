fa2b4357c8e7c7c1479c9f32dbba58746ecdf479
Darshan Jadhav <darshanjadhav363@gmail.com>
2024-05-18 15:45:08 +0530

    fix: removed cors policy

--- server/index.js
+++ server/index.js
@@ -5,13 +5,8 @@ const port = 3000
 const mongoose = require('mongoose');
 const tableRoutes = require('./routes/table')
 
-const corsOptions = {
-  origin:'http://localhost:5173',
-  methods:'GET, POST, PUT, DELETE',
-  Credential:true
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 
